5f35dbd9463599a0ea9913101edbb7898f37ba6b
Cisco29 <castro.francisco@live.fr>
2024-03-02 14:54:27 +0100

    téléchargement images et fix

---
+++ Portfolio-React2/src/components/navbar/Navbar.jsx
@@ -0,0 +1,20 @@
+import "./navbar.scss"
+
+const Navbar = () => {
+  return (
+    <div className="navbar">
+        {/* sidebar */}
+        <div className="wrapper">
+            <span>Cisco29</span>
+            <div className="social">
+                <a href="#"><img src="" alt=""/></a>
+
+            </div>
+
+        </div>
+
+    </div>
+  )
+}
+
+export default Navbar
