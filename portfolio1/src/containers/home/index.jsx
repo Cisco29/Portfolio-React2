0b3cf6655c41bb758dfde27ffa479208207816d9
Cisco29 <castro.francisco@live.fr>
2024-03-04 23:44:42 +0100

    Animation home page

--- portfolio1/src/containers/home/index.jsx
+++ portfolio1/src/containers/home/index.jsx
@@ -1,17 +1,35 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
+import { Animate } from "react-simple-animate";
 import "./styles.scss";
 
 const Home = () => {
-    return (
-       <section id="home" className="home">
-            <div className="home__text-wrapper">
-                <h1>Bonjour, je suis  Francisco Castro 
-                    <br />
-                    Développeur full-stack Junior
-                </h1>
+  const navigate = useNavigate();
 
-            </div>
-       </section>
-    );
+  const handleNavigateToContactMePage = () => {
+    navigate("/contact");
+  };
+  return (
+    <section id="home" className="home">
+      <div className="home__text-wrapper">
+        <h1>
+          Bonjour, je suis Francisco
+          <br />
+          Développeur full-stack Junior
+        </h1>
+      </div>
+      <Animate
+      play
+      duration={1.5}
+      delay={1}
+      start={{ transform: "translateY(550px)" }}
+      end={{ transform: "translateX(0px)" }}>
+
+        <div className="home__contact-me">
+          <button onClick={handleNavigateToContactMePage}>Hire Me</button>
+        </div>
+      </Animate>
+    </section>
+  );
 };
 export default Home;
