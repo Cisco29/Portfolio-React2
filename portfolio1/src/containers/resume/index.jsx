ce684f36bd716d148ed49c6fd1819fb3499dccb9
Cisco29 <castro.francisco@live.fr>
2024-03-11 12:20:42 +0100

    correction concernant l'affichage
    home about ect

--- portfolio1/src/containers/resume/index.jsx
+++ portfolio1/src/containers/resume/index.jsx
@@ -5,6 +5,7 @@ import { VerticalTimeline } from "react-vertical-timeline-component";
 import { data } from "./utils";
 import { VerticalTimelineElement } from "react-vertical-timeline-component";
 import 'react-vertical-timeline-component/style.min.css';
+import "./styles.scss";
 
 
 const Resume = () => {
@@ -43,8 +44,35 @@ const Resume = () => {
             </VerticalTimeline>
 
           </div>
-          <div className="timeline__education"></div>
+          <div className="timeline__education">
           <h3 className="timeline__education__header-text">Education</h3>
+          <VerticalTimeline
+            layout="1-column"
+            lineColor="var(--yellow-theme-main-color)"
+            >
+              {
+                data.education.map((item,i)=>(
+                  <VerticalTimelineElement
+                  key={i}
+                  className="timeline__experience__vertical-timeline-element"
+                  >
+                    <div className="vertical-timeline-element-title-wrapper">
+                      <h3 className="vertical-timeline-element-title">
+                      {item.title}
+                      </h3>
+                      <h4 className="vertical-timeline-element-subtitle">
+                      {item.subTitle}
+                      </h4>
+                      <p>{item.description}</p>
+
+                    </div>
+
+                  </VerticalTimelineElement> 
+                ))
+              }
+
+            </VerticalTimeline>
+         </div>
          </div>
       </section>
     );
