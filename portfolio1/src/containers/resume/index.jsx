51a0cbdee66b2f57457e09b9eedfc4c2be2f310c
Cisco29 <castro.francisco@live.fr>
2024-03-23 16:58:43 +0100

    ajout commentaire skill

--- portfolio1/src/containers/resume/index.jsx
+++ portfolio1/src/containers/resume/index.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 import PageHeaderContent from "../../components/PageHeaderContent";
 import { BsInfoCircleFill } from "react-icons/bs";
-import { VerticalTimeline } from "react-vertical-timeline-component";
+import { VerticalTimeline } from "react-vertical-timeline-component"; // Import du composant VerticalTimeline
 import { data } from "./utils"; // Import des données d'expérience et d'éducation
-import { VerticalTimelineElement } from "react-vertical-timeline-component";
-import 'react-vertical-timeline-component/style.min.css';
-import "./styles.scss";
-import { MdWork } from "react-icons/md";
+import { VerticalTimelineElement } from "react-vertical-timeline-component"; // Import d'éléments pour la timeline
+import 'react-vertical-timeline-component/style.min.css'; // Import des styles pour VerticalTimeline
+import "./styles.scss"; // Import des styles locaux
+import { MdWork } from "react-icons/md"; // Import de l'icône Work depuis react-icons/md
 
 const Resume = () => {
     return (
@@ -18,9 +18,10 @@ const Resume = () => {
             <div className="timeline">
                 <div className="timeline__experience">
                     <h3 className="timeline__experience__header-text">Experience</h3>
+                    {/* VerticalTimeline pour l'expérience */}
                     <VerticalTimeline
-                        layout="1-column"
-                        lineColor="var(--yellow-theme-main-color)"
+                        layout="1-column" // Définit la disposition à une colonne
+                        lineColor="var(--yellow-theme-main-color)" // Couleur de la ligne de la timeline
                     >
                         {data.experience.map((item, i) => (
                             <VerticalTimelineElement
@@ -32,7 +33,7 @@ const Resume = () => {
                                     border: "1.5px solid var(--yellow-theme-main-color)",
                                 }}
                                 date={`${item.startDate} - ${item.endDate}`} // Utilisez les dates spécifiques
-                                icon={<MdWork />}
+                                icon={<MdWork />} // Icône représentant le travail
                                 iconStyle={{
                                     background: '#181818',
                                     color: "var(--yellow-theme-main-color)",
@@ -65,7 +66,7 @@ const Resume = () => {
                                     border: "1.5px solid var(--yellow-theme-main-color)",
                                 }}
                                 date={`${item.startDate} - ${item.endDate}`} // Utilisez les dates spécifiques
-                                icon={<MdWork />}
+                                icon={<MdWork />} // Icône représentant le travail (à remplacer par une icône d'éducation si nécessaire)
                                 iconStyle={{
                                     background: '#181818',
                                     color: "var(--yellow-theme-main-color)",
